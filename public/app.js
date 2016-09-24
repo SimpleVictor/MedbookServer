e9b471da4160fd73a95433d4f4183e97d4634f00
Victor <viet.le175@gmail.com>
2016-09-24 10:39:16 -0400

    server

--- public/app.js
+++ public/app.js
@@ -1,1 +1,37 @@
-console.log(firebase);
+function patient_(firstName, lastName, age, sex, dob, phoneNumber, address, blood_type, condition, date_arrived, room_number) {
+    var num = Math.floor(Math.random()*9000) + 10000;;
+
+    var obj = {
+            firstName: firstName,
+            lastname: lastName,
+            age: age,
+            sex: sex,
+            address: address,
+            phoneNumber: phoneNumber,
+            blood_type: blood_type,
+        condition: condition,
+        data_arrived: date_arrived,
+        room_number : room_number
+        };
+
+
+    return obj
+
+
+}
+
+
+
+
+
+firebase.database().ref("patient_report/"+Math.round(Math.random()*9999)).set(patient_("Alexa",
+    "Smith",
+    "18",
+    "F",
+    "September 12 1998",
+    "+19089308704",
+    "123 redbull drive, New York",
+    "B-",
+    "stable",
+    Date.now(),
+    "43A"));
--- server/routes/login.ts
+++ server/routes/login.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response, NextFunction } from "express";
-
 const loginRouter: Router = Router();
+
+var firebase = require('firebase');
+var requests = require('request');
+var http = require('http');
 
 let echoData;
 
@@ -15,6 +18,26 @@ loginRouter.get("/grabData", function (request: Request, response: Response, nex
 loginRouter.post("/apidata", function (request: Request, response: Response, next: NextFunction) {
     console.log(request.body);
     echoData = request.body;
+
+
+    let myObject = {
+        loggedMobile: echoData
+    };
+
+    requests({
+        url: `https://echoproject-c786f.firebaseio.com/.json`,
+        method: "PATCH",
+        body: myObject,
+        json: true,
+    }, function (error, response){
+        if(error) {
+            console.log("BRRRRUHHHH THERE WAS AN ERROR");
+            console.log(error);
+        }else{
+            console.log("Successfully Changed Data");
+        }
+    });
+
     response.end();
 });
 
